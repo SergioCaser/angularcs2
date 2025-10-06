a8f6211e5fc7110dd0a906bc23c8572e2cb5229b
SergioCaser <sgonzalez@externos.caser.es>
2025-10-06 10:33:38 +0000

    primer commit 1

---
+++ src/app/parent/parent.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+//import { ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-parent',
+  imports: [],
+  templateUrl: './parent.html',
+  styleUrl: './parent.css',
+  //encapsulation: ViewEncapsulation.ShadowDom
+})
+export class Parent {
+
+}
