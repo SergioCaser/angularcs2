308ee5cedd33bd9f7128d44cf98a633281d0ffcd
sergio <sergio.illera@gmail.com>
2025-10-06 15:23:11 +0200

    commit 2 conent

--- src/app/parent/parent.ts
+++ src/app/parent/parent.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-//import { ViewEncapsulation } from '@angular/core';
+// import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-parent',
   imports: [],
   templateUrl: './parent.html',
   styleUrl: './parent.css',
-  //encapsulation: ViewEncapsulation.ShadowDom
+  // encapsulation: ViewEncapsulation.ShadowDom
 })
 export class Parent {
 
